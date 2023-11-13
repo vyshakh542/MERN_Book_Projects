e38f6faff3261b213df4d01720e2d3a00e41b04f
Vyshakh <vyshakh542@gmail.com>
2023-11-13 08:54:12 +0530

    payment section completed

--- mern-client/src/components/BookCards.jsx
+++ mern-client/src/components/BookCards.jsx
@@ -1,25 +1,117 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-// import './styles.css';
-
-// import required modules
 import { Pagination } from 'swiper/modules';
-import { Link } from 'react-router-dom';
-
-import {FaCartShopping} from "react-icons/fa6"
+import { FaCartShopping } from 'react-icons/fa6';
 
 const BookCards = ({ headline, books }) => {
-  // console.log(books)
+  const handleBookClick = (book) => {
+    // Open a new window with payment options
+    const paymentOptionsWindow = window.open('', '_blank');
+
+    // Render payment options content in the new window
+    paymentOptionsWindow.document.write(`
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: 'Arial', sans-serif;
+              background-color: #f5f5f5;
+              padding: 20px;
+              text-align: center;
+            }
+            h2 {
+              color: #333;
+            }
+            form {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              margin-top: 20px;
+            }
+            label {
+              display: block;
+              margin-bottom: 8px;
+              font-weight: bold;
+            }
+            input {
+              width: 300px;
+              padding: 10px;
+              margin-bottom: 12px;
+              box-sizing: border-box;
+              border: 1px solid #ddd;
+              border-radius: 5px;
+              font-size: 16px;
+            }
+            button {
+              background-color: #007BFF; /* Updated to use the specified color */
+              color: white;
+              padding: 12px;
+              border: none;
+              border-radius: 5px;
+              cursor: pointer;
+              font-size: 16px;
+            }
+            button:hover {
+              background-color: #0056b3;
+            }
+            .error {
+              color: #ff0000;
+              margin-top: 8px;
+            }
+            .success {
+              color: #008000;
+              margin-top: 8px;
+            }
+          </style>
+        </head>
+        <body>
+          <h2>Payment Options for - ${book.bookTitle}</h2>
+          <form id="paymentForm">
+            <label for="cardNumber">Card Number:</label>
+            <input type="text" id="cardNumber" name="cardNumber" placeholder="1234 5678 9012 3456" required>
+
+            <label for="expirationDate">Expiration Date:</label>
+            <input type="text" id="expirationDate" name="expirationDate" placeholder="MM/YYYY" required>
+
+            <label for="cvv">CVV:</label>
+            <input type="text" id="cvv" name="cvv" placeholder="123" required>
+
+            <button type="button" onclick="submitPayment()">Submit Payment</button>
+            <div id="error" class="error"></div>
+            <div id="success" class="success"></div>
+          </form>
+
+          <button onclick="window.close()" style="background-color: #007BFF; color: white; padding: 12px; border: none; border-radius: 5px; cursor: pointer; font-size: 16px;">Close</button>
+
+          <script>
+            function submitPayment() {
+              // You can add your payment processing logic here
+              const cardNumber = document.getElementById('cardNumber').value;
+              const expirationDate = document.getElementById('expirationDate').value;
+              const cvv = document.getElementById('cvv').value;
+
+              // Example: Validate the inputs
+              if (!cardNumber || !expirationDate || !cvv) {
+                document.getElementById('error').innerText = 'Please fill in all fields.';
+              } else {
+                // Example: Display success message
+                document.getElementById('error').innerText = '';
+                document.getElementById('success').innerText = 'Payment successful!';
+
+                // You can redirect or perform other actions here
+              }
+            }
+          </script>
+        </body>
+      </html>
+    `);
+  };
+
   return (
     <div className='my-16 px-4 lg:px-24'>
       <h2 className='text-5xl my-5 font-bold text-center'>{headline}</h2>
-      {/* cards */}
       <div className='mt-12'>
         <Swiper
           slidesPerView={1}
@@ -42,37 +134,34 @@ const BookCards = ({ headline, books }) => {
             },
           }}
           modules={[Pagination]}
-          className="mySwiper w-full h-full"
+          className='mySwiper w-full h-full'
         >
-          {
-            books.map(book => <SwiperSlide key={book._id}>
-              <Link to={`/book/${book._id}`}>
-                <div className='relative'>
-                  <img src={book.imageURL} alt="" />
-
-                  <div className='absolute top-3 right-3 bg-blue-700 hover:bg-black p-2 rounded '>
-                    <FaCartShopping className='w-4 h-4 text-white'/>
-                  </div>
-
+          {books.map((book) => (
+            <SwiperSlide key={book._id}>
+              <div className='relative'>
+                <img src={book.imageURL} alt='' />
+                <div
+                  className='absolute top-3 right-3 bg-blue-700 hover:bg-black p-2 rounded cursor-pointer'
+                  onClick={() => handleBookClick(book)}
+                >
+                  <FaCartShopping className='w-4 h-4 text-white' />
                 </div>
+              </div>
+              <div>
                 <div>
+                  <h3>{book.bookTitle}</h3>
+                  <p>{book.authorName}</p>
                   <div>
-                    <h3>{book.bookTitle}</h3>
-                    <p>{book.authorName}</p>
-                    <div>
-                      <p>$10.00</p>
-                    </div>
+                    <p>$10.00</p>
                   </div>
                 </div>
-
-
-              </Link>
-            </SwiperSlide>)
-          }
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BookCards
+export default BookCards;
--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -1,43 +1,127 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Card } from 'flowbite-react';
 
-const shop = () => {
+const Shop = () => {
   const [books, setBooks] = useState([]);
-  useEffect( () => {
-    fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data));
-  }, [])
-  // })
+  const [showPaymentModal, setShowPaymentModal] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
 
+  useEffect(() => {
+    fetch("http://localhost:5000/all-books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
 
+  const handleBuyNow = (book) => {
+    setSelectedBook(book);
+    setShowPaymentModal(true);
+  };
 
+  const closePaymentModal = () => {
+    setShowPaymentModal(false);
+    setSelectedBook(null);
+  };
+
+  const submitPayment = () => {
+    // Add your payment processing logic here
+    // You can use selectedBook for details about the book being purchased
+    // Display success or error messages accordingly
+    // This is a placeholder, update it based on your payment processing needs
+    console.log('Processing payment for:', selectedBook);
+    // Clear selected book and close modal after processing payment
+    setSelectedBook(null);
+    setShowPaymentModal(false);
+  };
 
   return (
     <div className='mt-28 px-4 lg:px24'>
-      <h2 className='text-5xl font-bold text-center '>All Books are Available Here</h2>
+      <h2 className='text-5xl font-bold text-center'>All Books are Available Here</h2>
 
       <div className='grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1'>
-          {
-            books.map(book => <Card>
-              <img src={book.imageURL} alt="" className='h-96' />
-              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <p>
-                  {book.bookTitle}
-                </p>
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                <p>
-                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order....
-                </p>
-              </p>
+        {books.map((book) => (
+          <Card key={book._id}>
+            <img src={book.imageURL} alt='' className='h-96' />
+            <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              <p>{book.bookTitle}</p>
+            </h5>
+            <p className='font-normal text-gray-700 dark:text-gray-400'>
+              <p>{/* Description or additional information about the book */}</p>
+            </p>
 
-              <button className='bg-blue-700 font-semibold text-white py-2 rounded'>Buy Now</button>
-            </Card>)
-          }
+            <button
+              className='bg-blue-700 font-semibold text-white py-2 rounded'
+              onClick={() => handleBuyNow(book)}
+            >
+              Buy Now
+            </button>
+          </Card>
+        ))}
+      </div>
+
+      {/* Payment Modal */}
+      {showPaymentModal && selectedBook && (
+        <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
+          <div className='bg-white p-8 rounded shadow-lg'>
+            <h2 className='text-2xl font-bold mb-4'>
+              Payment Options for {selectedBook.bookTitle}
+            </h2>
+            {/* Add your payment options UI here */}
+            <div className='flex items-center justify-between mb-4'>
+              <label htmlFor='cardNumber' className='mr-2'>
+                Card Number:
+              </label>
+              <input
+                type='text'
+                id='cardNumber'
+                name='cardNumber'
+                className='p-2 border rounded'
+                placeholder='1234 5678 9012 3456'
+                required
+              />
+            </div>
+            <div className='flex items-center justify-between mb-4'>
+              <label htmlFor='expirationDate' className='mr-2'>
+                Expiration Date:
+              </label>
+              <input
+                type='text'
+                id='expirationDate'
+                name='expirationDate'
+                className='p-2 border rounded'
+                placeholder='MM/YYYY'
+                required
+              />
+            </div>
+            <div className='flex items-center justify-between mb-4'>
+              <label htmlFor='cvv' className='mr-2'>
+                CVV:
+              </label>
+              <input
+                type='text'
+                id='cvv'
+                name='cvv'
+                className='p-2 border rounded'
+                placeholder='123'
+                required
+              />
+            </div>
+            <button
+              className='bg-blue-500 text-white p-2 rounded'
+              onClick={submitPayment}
+            >
+              Submit Payment
+            </button>
+            <button
+              className='ml-2 text-gray-600'
+              onClick={closePaymentModal}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-      
-
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default shop
+export default Shop;
