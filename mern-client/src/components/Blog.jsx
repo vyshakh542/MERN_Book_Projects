8439bde917899ffdada03492c3f9e90c7894ec80
Vyshakh <vyshakh542@gmail.com>
2023-11-13 13:37:28 +0530

    blog page completed

--- mern-client/src/components/Blog.jsx
+++ mern-client/src/components/Blog.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const Blog = () => {
+const About = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="max-w-2xl mx-auto text-center">
+        <h1 className="text-4xl font-bold mb-4 text-blue-700">Welcome to Our Bookstore!</h1>
+        <p className="text-gray-700">
+          Dive into the world of literature with us at bookstore.com, where every page
+          holds the promise of adventure, knowledge, and discovery. Our mission is to
+          bring the magic of storytelling to readers worldwide.
+        </p>
+        <p className="text-gray-700 mt-4">
+          Our online bookstore is more than just a collection of books; it's a carefully
+          curated selection designed to cater to diverse tastes and interests. Whether
+          you're a seasoned book enthusiast, a casual reader, or on the hunt for the
+          perfect gift, we've created an immersive experience just for you.
+        </p>
+        <p className="text-gray-700 mt-4">
+          Navigate through our user-friendly platform, where you can explore the latest
+          releases, uncover hidden literary gems, and connect with stories that resonate
+          with you. Thank you for choosing bookstore.com as your literary companion.
+          Embark on a journey of stories, knowledge, and imagination. Happy reading!
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default About;
